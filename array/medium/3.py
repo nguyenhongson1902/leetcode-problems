6964f2744dbf923b1b09559c0e4799a75a3c04f0
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-06-12 00:54:07 +0700

    Longest Substring Without Repeating Characters

--- array/medium/3.py
+++ array/medium/3.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Longest Substring Without Repeating Characters
 
+# The first time
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) == 0:
@@ -17,3 +18,43 @@ class Solution:
             end += 1
         
         return max_length
+    
+# The second time
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+
+        left, right = 0, 0
+        max_len = 0
+        char_set = set()
+        while right < len(s):
+            curr_len = right - left + 1
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                max_len = max(max_len, curr_len)
+                right += 1
+            else:
+                char_set.remove(s[left])
+                left += 1
+            
+        return max_len
+    # Time complexity: O(n), n = len(s)
+    # Space complexity: O(n), worst case: every character is UNIQUE
+
+    # if len(s) <= 1:
+        #     return len(s)
+
+        # left, right = 0, 0
+        # max_len = 0
+        # char_set = set()
+        # while right < len(s):
+        #     while s[right] in char_set:
+        #         char_set.remove(s[left])
+        #         left += 1
+        #     curr_len = right - left + 1
+        #     char_set.add(s[right])
+        #     max_len = max(max_len, curr_len)
+        #     right += 1
+            
+        # return max_len

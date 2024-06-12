11343a6a2567b4570a74d9e64af19c349c44dbd4
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-06-12 11:55:04 +0700

    Longest Repeating Character Replacement

---
+++ array/medium/424.py
@@ -0,0 +1,34 @@
+# Longest Repeating Character Replacement
+# https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left, right = 0, 0
+        max_len = 0
+        freq = {}
+        right_increased = False
+        while right < len(s):
+            curr_len = right - left + 1
+            if s[right] not in freq:
+                freq[s[right]] = 1
+            else:
+                if right_increased:
+                    freq[s[right]] += 1
+            # print("freq", freq)
+            # print("left", left, "right", right)
+            max_freq_char = max(freq, key=freq.get) # most occurred letter
+            max_freq = freq[max_freq_char] # most occurrences
+            if curr_len - max_freq <= k:
+                max_len = max(max_len, curr_len)
+                right += 1
+                right_increased = True
+            else:
+                freq[s[left]] -= 1
+                left += 1
+                right_increased = False
+        return max_len
+        # Time complexity: O(n), n = len(s)
+        
+        
+

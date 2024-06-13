f83ff2b3f68f04d29a31630879d6b60434caa5c4
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-06-13 11:26:09 +0700

    Valid Palindrome

--- array/easy/125.py
+++ array/easy/125.py
@@ -2,6 +2,7 @@
 # Valid Palindrome
 
 
+# The first time
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s_new = ""
@@ -16,3 +17,19 @@ class Solution:
             begin += 1
             end -= 1
         return True
+
+# The second time
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        tmp = s.lower()
+        tmp = "".join(tmp.split(" "))
+        tmp = "".join([c for c in tmp if c.isalnum()])
+
+        left, right = 0, len(tmp) - 1
+        while left <= right and tmp[left] == tmp[right]:
+            left += 1
+            right -= 1            
+        
+        return True if left > right else False
+        # Time complexity: O(n), n = len(s)
+        # Space complexity: O(n), n = len(s) because we use a temporarily processed string

0164bd93b172117c3f214b3dfdb19b5060ae70b3
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-06-23 02:01:36 +0700

    Two Sum

--- array/easy/1.py
+++ array/easy/1.py
@@ -6,20 +6,20 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # hash_map = {}
-        # for idx, num in enumerate(nums):
-        #     hash_map[target - num] = idx
+        hash_map = {}
+        for idx, num in enumerate(nums):
+            hash_map[target - num] = idx
         
-        # for idx, num in enumerate(nums):
-        #     if num in hash_map and idx != hash_map[num]:
-        #         return [idx, hash_map[num]]
+        for idx, num in enumerate(nums):
+            if num in hash_map and idx != hash_map[num]:
+                return [idx, hash_map[num]]
         # Time complexity: O(n), n = len(nums)
         # Space complexity: O(n), because we're using a hash map
 
         # One-pass solution:
-        hash_map = {}
-        for idx, num in enumerate(nums):
-            if target - num in hash_map:
-                return [idx, hash_map[target - num]]
-            else:
-                hash_map[num] = idx
+        # hash_map = {}
+        # for idx, num in enumerate(nums):
+        #     if target - num in hash_map:
+        #         return [idx, hash_map[target - num]]
+        #     else:
+        #         hash_map[num] = idx

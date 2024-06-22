21eaca1e115c92df37ad779c7d1ff246adc00594
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-06-22 17:08:43 +0700

    Subsets II

---
+++ backtracking/medium/90.py
@@ -0,0 +1,27 @@
+# Subsets II
+# https://leetcode.com/problems/subsets-ii/
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums = sorted(nums)
+        subset = []
+        def backtrack(i):
+            if i == len(nums):
+                result.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1)
+
+            subset.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+
+            backtrack(i + 1)
+        
+        backtrack(0)
+        return result
+        # Time: O(n*2^n), n = len(nums)
